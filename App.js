8c5adb6bed3d9745f5b89a79da42295f7f96c6e7
JohnyDeCoder <johnymora251@gmail.com>
2023-11-08 10:53:44 -0600

    Added full project

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Router, Scene } from "react-native-router-flux";
+import LoginView from "./src/LoginView";
+import RegisterView from "./src/RegisterView";
+
+const App = () => {
+  return (
+    <Router>
+      <Scene key="root">
+        <Scene
+          key="login"
+          component={LoginView}
+          title="Login"
+          initial={true}
+          hideNavBar
+        />
+        <Scene key="register" component={RegisterView} title="Register" />
+      </Scene>
+    </Router>
+  );
+};
+
+export default App;
